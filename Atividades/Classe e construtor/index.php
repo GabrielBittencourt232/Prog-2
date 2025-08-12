6e3e3bf67ad2ddb9272b47f35b45a454c17350c9
Gabriel <gabrielbittencourt1036@gmail.com>
2025-08-11 21:53:15 -0300

    atividades

---
+++ Atividades/Classe e construtor/index.php
@@ -0,0 +1,11 @@
+<?php
+class Pessoa{
+    public $nome;
+    public $idade;
+
+    function apresentar(){} 
+
+    function __construct($nome, $idade){}
+
+}
+?>
---
+++ Atividades/Visibilidade/index.php
@@ -0,0 +1,13 @@
+<?php
+class Pessoa{
+    private $nome;
+    private $idade;
+
+    function getNome($nome){}
+    function getIdade($idade){}
+    
+    function apresentar(){} 
+
+    function __construct($nome, $idade){}
+}
+?>
